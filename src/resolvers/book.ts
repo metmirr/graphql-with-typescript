dcfcaf64b531a3c3766ff008b0232deafd61d785
metmirr <metmirr@gmail.com>
2020-04-02 17:48:03 +0300

    Send notification on new book creation

--- src/resolvers/book.ts
+++ src/resolvers/book.ts
@@ -1,7 +1,16 @@
-import { Resolver, Query, Mutation, Arg, Args } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  Args,
+  PubSub,
+  PubSubEngine
+} from "type-graphql";
 import { Book } from "../entities/Book";
 import { CreateBook } from "../inputs/CreateBook";
 import { GetBookArgs } from "../args/Book";
+import { NotificationPayload } from "../notifications/book";
 
 @Resolver(of => Book)
 export class BookResolver {
@@ -14,9 +23,19 @@ export class BookResolver {
   }
 
   @Mutation(returns => Book)
-  async createBook(@Arg("data") data: CreateBook) {
+  async createBook(
+    @Arg("data") data: CreateBook,
+    @PubSub() pubSub: PubSubEngine
+  ) {
     const book = Book.create(data);
     await book.save();
+
+    const payload: NotificationPayload = {
+      id: book.id,
+      message: `A new book published! The book has ${book.title} title`
+    };
+    await pubSub.publish("NOTIFICATIONS", payload);
+
     return book;
   }
 }
